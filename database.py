c3e34107e1ecbd8e57c7da5a78ae03bc33633f14
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-14 11:47:32 -0600

    db tables passed to database classes constructor

--- database.py
+++ database.py
@@ -3,11 +3,12 @@ from playhouse.db_url import connect
 
 class Database:
 
-    def __init__(self):
+    def __init__(self, db_tables):
         self.DATABASE = SqliteDatabase('foodsite.sqlite')
+        self.db_tables = db_tables
 
     # called when the server starts so the db tables are created
     def initialize_tables(self):
         self.DATABASE.connect()
-        self.DATABASE.create_tables([])
+        self.DATABASE.create_tables([self.db_tables])
         self.DATABASE.close()
