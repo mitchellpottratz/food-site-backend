4100d2bcfc055232c3ac3bd0eb19724df9c3bbaa
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-14 09:54:38 -0600

    setup spring application

---
+++ src/main/java/com/weblinktech/foodsitebackend/FoodSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weblinktech.foodsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSiteBackendApplication.class, args);
+	}
+
+}
