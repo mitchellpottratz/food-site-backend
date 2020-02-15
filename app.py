3d28ae349c816c59438d20d7ed054a4ee0f51be9
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-15 10:57:20 -0600

    changed users controller import to correct name

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from flask import Flask
 # server and databased
 from server import Server
 from database import Database
-server = Server(True, 3000)
-database = Database([])
 
 # resource imports
-from resources.users import 
+from resources.users import users
 
 from flask_login import LoginManager
 
 app = Flask(__name__)
+
+server = Server(True, 3000)
+database = Database([])
 
 # api endpoint route
 app.register_blueprint(User, url_prefix='/api/v1/users')
