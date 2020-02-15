f898728013d0962e9639e4ddfb40bc66698a2578
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-15 13:22:10 -0600

    apps origin is set

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ class Server:
         self.login_manager = LoginManager()
         self.DEBUG = DEBUG
         self.PORT = PORT
+        self.origin = self.set_origin()
 
         # sets the apps secret key
         self.app.secret_key = os.environ['SECRET_KEY']
@@ -22,12 +23,20 @@ class Server:
         # sets up the login manager
         self.setup_login_manager()
 
+    # sets which url is able to access this application
+    def set_origin(self):
+        if self.DEBUG:
+            return 'http://localhost:3000'
+        else:
+            return os.environ['ORIGIN']
+
     # sets up flask_login
     def setup_login_manager(self):
         self.login_manager.init_app(self.app)
 
     def register_blueprint(self, resource, path):
         self.app.register_blueprint(resource, url_prefix=path) 
+
 
     def start(self): 
         self.app.run(debug=self.DEBUG, port=self.PORT)
