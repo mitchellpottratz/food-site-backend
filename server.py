49e593f6a75c7c6da393daaffcc6729d28458617
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-15 12:32:44 -0600

    added python-dotenv to requirements

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
 import os 
 from flask import Flask
+from flask_login import LoginManager, current_user
 
 class Server:
 
     def __init__(self, DEBUG, PORT):
         self.app = Flask(__name__)
+        self.login_manager = LoginManager()
         self.DEBUG = DEBUG
         self.PORT = PORT
 
+        # sets the apps secret key
+        self.app.secet_key = self.set_secret_key()
+
+        # sets up the login manager
+        self.setup_login_manager()
+
+    def set_secret_key(self):
+        return os.environ['SECRET_KEY']
+
+    def setup_login_manager(self):
+        self.login_manager.init_app(self.app)
+
     def register_blueprint(self, resource, path):
         self.app.register_blueprint(resource, url_prefix=path) 
+
 
     def start(self): 
         self.app.run(debug=self.DEBUG, port=self.PORT)
