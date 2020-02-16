c08ec11351fa6a915911dac969882f3c583e214b
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-16 12:56:29 -0600

    uninstalled requests

--- resources/restaurants.py
+++ resources/restaurants.py
@@ -2,6 +2,9 @@ import os
 from flask import request, jsonify, Blueprint
 from peewee import DoesNotExist
 
+# handles request for 3rd party web service
+from urllib.request import urlopen 
+
 # blueprint for Restaurant
 restaurants = Blueprint('user', 'user')
 
@@ -11,7 +14,8 @@ all_restaurants = os.environ['API_KEY']
 # route to list all restaurants from api
 @restaurants.route('/allRestaurants', methods=["GET"])
 def list_restaurants():
-    data = requests.get('https://eatstreet.com/' + all_restaurants + '/v1/restaurant/search?method=both&pickup-radius=100&street-address=520+W+Taylor+St.,+Chicago,+IL')
-    return data.text
+    with urlopen('https://eatstreet.com/' + all_restaurants + '/v1/restaurant/search?method=both&pickup-radius=100&street-address=520+W+Taylor+St.,+Chicago,+IL') as r:
+        text = r.read()
+    return text
 
     
