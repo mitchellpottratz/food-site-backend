f9c662da9f6be90ef59de22bf212a71151e79bc7
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-17 14:35:30 -0600

    removed extra fields from address model

--- models/address.py
+++ models/address.py
@@ -4,17 +4,13 @@ from .user import User
 
 ''' 
 This model is a a delivery address the user can save. Users are able to have multiple
-delivery addresses which they can give names to
+delivery addresses which can be given a name.
 '''
 
 class Address(BaseModel):
     user = ForeignKeyField(User, backref='addresses')
     name = CharField(max_length=55)
-    state = CharField(max_length=25)
-    city = CharField(max_length=55)
-    street = CharField(max_length=155)
-    house_number = IntegerField(null=True)
-    apartment_number = IntegerField(null=True)
+    address = CharField(max_length=255)
     instructions = CharField(max_length=300)
 
 
