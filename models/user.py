142bdc41ea2b464c2a242ff94ba531a41236ad74
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-17 12:31:03 -0600

    User model now inherits from the base model

--- models/user.py
+++ models/user.py
@@ -1,19 +1,12 @@
-import os
-import datetime
-
 from peewee import *
 from flask_login import UserMixin
+from .base import BaseModel
 
-DATABASE = SqliteDatabase('foodsite.sqlite')
 
-class User(UserMixin, Model):
+class User(BaseModel, UserMixin):
     first_name = CharField()
     last_name = CharField()
     email = CharField()
     password = CharField()
     active = BooleanField(default=False)
-    last_updated = DateTimeField(default=datetime.datetime.now)
-    timestamp = DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        database = DATABASE
+    
