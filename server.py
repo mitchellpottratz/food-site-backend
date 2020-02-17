d8ef51dab8e4dd305f8e24d9d4887f881bf1df11
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-17 14:00:20 -0600

    configured cors with flask app -- elimated cors error on front end

--- server.py
+++ server.py
@@ -2,24 +2,28 @@ import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_cors import CORS
-
 # allows flask to access enviroment variables
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env') 
 load_dotenv(dotenv_path)
-
-
 class Server:
-
     def __init__(self, DEBUG, PORT, blueprints):
+
         self.app = Flask(__name__)
+
         self.login_manager = LoginManager()
+
         self.DEBUG = DEBUG
+
         self.PORT = PORT
+
         self.origin = self.set_origin()
 
         # sets the apps secret key
         self.app.secret_key = os.environ['SECRET_KEY']
+
+        # sets up cors configuration for the flask app
+        self.setup_cors()
 
         # sets up the login manager
         self.setup_login_manager()
@@ -32,24 +36,31 @@ class Server:
 
     # sets which url is able to access this application
     def set_origin(self):
+
         if self.DEBUG:
-            return 'http://localhost:3000'
+            return 'http://localhost:3000/'
+
         else:
             return os.environ['ORIGIN']
 
+    def setup_cors(self):
+        
+        CORS(self.app)
+
     # sets up flask_login
     def setup_login_manager(self):
+
         self.login_manager.init_app(self.app)
 
     # loops through all of the blueprints and registers them with the application
     def register_blueprints(self):
+
         for blueprint in self.blueprints:
+
             self.app.register_blueprint(blueprint[0], url_prefix=blueprint[1])
             CORS(blueprint[0], origins=[self.origin], supports_credentials=True)
 
-
     def start(self): 
+        
         self.app.run(debug=self.DEBUG, port=self.PORT)
         print("Server is running on port", self.PORT)
-
-    
