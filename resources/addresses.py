1f00e85839dc3966c4b43ad459901036a434d8d2
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-17 17:35:02 -0600

    create address route works

--- resources/addresses.py
+++ resources/addresses.py
@@ -23,11 +23,25 @@ def ping():
 
 
 # Create Route
+# this route is where users can create a new delivery address
 @addresses.route('/', methods=['POST'])
 @login_required
 def create_address():
     data = request.get_json()
 
-    try:
-        
+    # attach the users id to the dictionary to specify which user is creating the address
+    data['user'] = current_user.id 
+
+    new_address = Address.create(**data)
+    new_address_dict = model_to_dict(new_address)
+
+    return jsonify(
+        data=new_address_dict,
+        status={
+            'code': 201,
+            'message': 'New delivery address created'
+        }
+    )
+
+
 
