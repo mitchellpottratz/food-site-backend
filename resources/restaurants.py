1b1ce883cd3fbea7bd93a67ae48978f4dd0f9f91
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-16 21:53:53 -0600

    restaurant index route successfully loads all products

--- resources/restaurants.py
+++ resources/restaurants.py
@@ -1,21 +1,25 @@
 import os
 from flask import request, jsonify, Blueprint
 from peewee import DoesNotExist
+from playhouse.shortcuts import model_to_dict
 
-# handles request for 3rd party web service
-from urllib.request import urlopen 
+# handles requests from 3rd party api
+import requests
+import json
 
 # blueprint for Restaurant
 restaurants = Blueprint('user', 'user')
 
-# how to obtain keys out of .env file
-all_restaurants = os.environ['API_KEY']
+# how to obtain keys out of .env file (API_KEY)
+all_restaurants_token = os.environ['API_KEY']
 
 # route to list all restaurants from api
-@restaurants.route('/allRestaurants', methods=["GET"])
+@restaurants.route('/', methods=["GET"])
 def list_restaurants():
-    with urlopen('https://eatstreet.com/' + all_restaurants + '/v1/restaurant/search?method=both&pickup-radius=100&street-address=520+W+Taylor+St.,+Chicago,+IL') as r:
-        text = r.read()
-    return text
-
-    
+    response = requests.get("https://eatstreet.com/publicapi/v1/restaurant/search?method=both&pickup-radius=100&street-address=520+W+Taylor+St.,+Chicago,+IL", headers={'X-Access-Token': all_restaurants_token})
+    try:
+        data = response.json()
+        return jsonify(data=data['restaurants'], status={"code": 200, "message": "successfully loaded all restaurants"}), 200
+    except:
+        # return error message if data cannot be processed 
+        return jsonify(data={}, status={'code': 500,'message': 'error loading all restaurants'}), 500
