3fae0c1800e60e693c552c4bdd49bcf843e720db
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-19 10:42:44 -0600

    added method to cart model to check if user is the cart owner

--- models/cart.py
+++ models/cart.py
@@ -6,3 +6,9 @@ from .user import User
 class Cart(BaseModel):
     user = ForeignKeyField(User, backref='cart', unique=True)
 
+    # checks if the user id in the parameters is the user of the model instance
+    def user_is_owner(self, user_id):
+        if self.user.id != user_id:
+            return False
+        return True
+
--- resources/carts.py
+++ resources/carts.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from playhouse.shortcuts import model_to_dict
 from peewee import DoesNotExist
+from exceptions.resource_access_denied import ResourceAccessDenied
 
 from models.cart import Cart 
 from models.user import User  
@@ -38,6 +39,26 @@ def create_users_cart():
             'message': 'Successfully created resource.'
         }
     )  
+
+
+# Delete Route
+# this route is where a users cart can be deleted
+@carts.route('/<card_id>', methods=['DELETE'])
+@login_required
+def delete_users_cart(cart_id):
+    try:
+        cart = Cart.get(id=cart_id):
+
+
+    except DoesNotExist:
+        return jsonify(
+            data={},
+            status={
+                'code': 403,
+                'message': 'Resource does not exist'
+            }
+        )
+
 
       
 
