276988a9c309077ba9543768a27fcb5ea2b9ce54
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-18 22:50:43 -0600

    added api_key and description fields to food item model

--- models/food_item.py
+++ models/food_item.py
@@ -5,10 +5,13 @@ from .cart import Cart
 
 class FoodItem(BaseModel):
     cart = ForeignKeyField(Cart, backref='food-items')
+    api_key = CharField(max_length=55)
     name = CharField(max_length=155)
+    description = CharField(max_length=500)
     price = DecimalField()
 
-    
+
+
 
 
 
