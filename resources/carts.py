fb8fc86ba65868478963abc8495b2ea478de0f93
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-19 10:37:20 -0600

    created the carts create route

--- resources/carts.py
+++ resources/carts.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from playhouse.shortcuts import model_to_dict
 from peewee import DoesNotExist
-from exceptions.resource_access_denied import ResourceAccessDenied
 
 from models.cart import Cart 
 from models.user import User  
@@ -23,6 +22,25 @@ def ping():
 
 
 # Create Route
+# this route creates a new cart for a user
+@carts.route('/', methods=['POST'])
+@login_required
+def create_users_cart():
+    new_cart = Cart.create(user=current_user.id)
+
+    new_cart_dict = model_to_dict(new_cart)
+    del new_cart_dict['user']['password']
+
+    return jsonify(
+        data=new_cart_dict,
+        status={
+            'code': 201,
+            'message': 'Successfully created resource.'
+        }
+    )  
+
+      
+
 
 
 
