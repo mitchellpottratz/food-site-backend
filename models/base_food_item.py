df80ddb9ffe89abeddbc427836d983e67acea962
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-21 13:41:58 -0600

    added method to fetch menus and food item from the api

--- models/base_food_item.py
+++ models/base_food_item.py
@@ -17,6 +17,25 @@ class BaseFoodItem(BaseModel):
     price = DecimalField()
     instructions = CharField(max_length=500)
 
+    # the api url to search for a restaurants menu
+    API_URL = 'https://eatstreet.com/publicapi/v1/restaurant/'
+
+    # header that needs to be sent in the request to the EatStreet api
+    API_HEADER = {'X-Access-Token': os.environ['API_KEY']}
+   
+    # makes a request to the EatStreet api to get all of the food item on a restaurants menu
+    @staticmethod
+    def get_restaurants_menu(restaurant_api_key):
+        response = requests.get(FoodItem.API_URL + restaurant_api_key +
+                                '/menu', headers=FoodItem.API_HEADER)
+        parsed_response = response.json()
+        return parsed_response
+
+    # parses the restaurants menu and returns the matching food item
+    @staticmethod
+    def get_food_item(menu, food_item_api_key):
+        pass
+
 
 
 
