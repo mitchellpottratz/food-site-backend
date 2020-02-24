3ce19b8add0cd47bb98a4ec0fc6bf50fdc60f0b1
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-24 14:26:13 -0600

    updated api key fields

--- models/food_item_customization.py
+++ models/food_item_customization.py
@@ -11,7 +11,8 @@ This model respresents a food item customization from the EatStreet API
 
 class FoodItemCustomization(BaseModel):
     food_item = ForeignKeyField(FoodItem, backref='customizations')
-    api_key = CharField(max_length=155)
+    selection_api_key = CharField(max_length=155)
+    choice_api_key = CharField(max_length=155)
     name = CharField(max_length=155)
     price = FloatField()
     count = IntegerField(null=True)
