32edbd262d7092d2b86a57c3effe69ce3412b509
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-28 10:20:20 -0600

    added importerror exception to elasticsearch instantiation

--- resources/elastic.py
+++ resources/elastic.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 from flask import Flask, jsonify, request, Blueprint
 from elasticsearch import Elasticsearch
+import warnings
 
 # initializes elasticsearch and flask modules
-es = Elasticsearch()
-# app = Flask(__name__)
+
+try:
+    es = Elasticsearch()
+except ImportError:
+    warnings.warn('Elasticsearch is not installed.')
+
+
 
 # # blueprint for User
 elastic = Blueprint('elastic', 'elastic')
