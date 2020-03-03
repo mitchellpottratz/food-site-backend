bfc3e1ba8e5079cdabf788fac0409f029996f6a2
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-03 15:39:54 -0600

    made route to find all nearby restaurants in elasticsearch

--- resources/elastic.py
+++ resources/elastic.py
@@ -25,3 +25,53 @@ def all_restaurants():
 
 
 
+# gets all restaurants within approximately 20 miles of the users location
+@elastic.route('/restaurants/nearme', methods=['GET'])
+def get_restaurants_near_user():
+
+    # gets the users location
+    clients_latitude = float(request.args.get('latitude'))
+    clients_longitude = float(request.args.get('longitude'))
+
+    results = elasticsearch_client.search(
+        index='restaurants',
+        body={
+            'query': {
+                'bool': {
+                    'must': {
+                        'range': {
+                            'latitude': {
+                                'lte': str(clients_latitude + 0.3),
+                                'gte': str(clients_latitude - 0.3),
+                            },
+                        }
+                    },
+                    'must': {
+                        'range': {
+                            'longitude': {
+                                'lte': str(clients_longitude + 0.3),
+                                'gte': str(clients_longitude - 0.3)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    ) 
+
+    return jsonify(
+        data=results['hits']['hits'],
+        status={
+            'code': 200,
+            'message': 'Successfully found resources'
+        }
+    )
+
+
+
+
+
+
+
+
+
